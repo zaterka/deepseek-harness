5d9af98780b8f956c0284d737ac6de74ddc3044c
zaterka <pedrozaterka@gmail.com>
2026-09-03 10:26:50 -0300

    feat(modes): added custom developer mode

--- packages/client/ui-sidebar/src/client/SidebarRoot.tsx
+++ packages/client/ui-sidebar/src/client/SidebarRoot.tsx
@@ -143,7 +143,7 @@ export function SidebarRoot({
                 {renderSlot('sidebar.brand.name', {}, {
                   fallback: (
                     <>
-                      <span className={css.fallbackBrandName}>DSH Local Build</span>
+                      <span className={css.fallbackBrandName}>DSH Version Z</span>
                       {process.env.DSH_CLIENT_COMMIT_HASH
                         ? <span className={css.buildRevision}>{process.env.DSH_CLIENT_COMMIT_HASH}</span>
                         : null}
